59d9d9f3053a14c4a446e30395cdb95c9b62f027
Ahsan Javed <ahsannjavaid@gmail.com>
2025-06-11 20:41:37 +0500

    My Resume Added

--- app/sections/Nav.tsx
+++ app/sections/Nav.tsx
@@ -1,34 +1,49 @@
-// import { useState } from 'react';
-import Link from 'next/link';
-import * as React from 'react';
-import Image from 'next/image';
-// import { FaBars } from 'react-icons/fa';
+import { useState } from "react";
+import Link from "next/link";
+import * as React from "react";
+import Image from "next/image";
+import { FaBars } from "react-icons/fa";
 
 const Nav = () => {
-  // const [navbarOpen, setNavbarOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className='relative flex flex-wrap items-center justify-between p-4 z-50 bg-[#202020]'>
-      <div className='container mx-auto flex flex-wrap items-center justify-between'>
-        <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
-          <Link href='/'>
-            <div className='text-lg font-bold flex justify-center gap-3 items-center mr-4 uppercase text-white'>
+    <nav className="relative flex flex-wrap items-center justify-between p-4 z-50 bg-[#202020]">
+      <div className="container mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <Link href="/">
+            <div className="text-lg font-bold flex justify-center gap-3 items-center mr-4 uppercase text-white">
               <Image
-                src={'/icons/icon-square.png'}
+                src={"/icons/icon-square.png"}
                 width={40}
                 height={40}
-                alt='ahsan-javed-logo'
-                className='rounded-xl'
+                alt="ahsan-javed-logo"
+                className="rounded-xl"
               />
-              <span className='tracking-wider'>Ahsan Javed</span>
+              <span className="tracking-wider">Ahsan Javed</span>
             </div>
           </Link>
-          {/* <button
-            className='text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
-            type='button'
+          <button
+            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
             onClick={() => setNavbarOpen(!navbarOpen)}
           >
             <FaBars />
-          </button> */}
+          </button>
+        </div>
+        <div
+          className={
+            "lg:flex flex-grow items-center" +
+            (navbarOpen ? " flex pt-5" : " hidden")
+          }
+          id="example-navbar-danger"
+        >
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <li className="nav-item">
+              <Link href="/files/Ahsan Javed Resume V0.1.pdf" target="_blank" rel="noopener noreferrer" className="py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:opacity-75">
+                My Resume
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
